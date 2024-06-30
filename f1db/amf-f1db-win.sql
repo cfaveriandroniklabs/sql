001ad8c72cf9adb4e786a2a084ab9990fbc7c53f
Cristiano De Faveri <cristiano@androniklabs.com.br>
2024-06-30 12:49:40 -0300

    Respostas f1db

--- f1db/amf-f1db-win.sql
+++ f1db/amf-f1db-win.sql
@@ -1,3 +1,5 @@
+-- Questão 1
+
 --Crie um banco de dados de nome f1;
 create database f1;
 --Renomeie o banco de dados para f1db;
@@ -11,11 +13,6 @@ create schema core;
 --Coloque o esquema core como padrão (default);
 set search_path to core, public;
 --Crie uma tabela no esquema core chamada team contendo os seguintes dados:
-
-drop table if exists core.team cascade;
-drop table if exists core.pilot cascade;
-drop table if exists core.race cascade;
-drop table if exists core.race_time cascade;
 
 create table core.team (
  	team_id serial primary key, 
@@ -56,24 +53,91 @@ create table race_time (
 );
 
 -- Inserindo dados na tabela core.team para todas as equipes de Fórmula 1 da temporada atual
-copy core.team (team_id, name, country) from 'd:\temp\f1teams.csv' csv header delimiter ';';
+copy core.team (team_id, name, country) from '/tmp/f1teams.csv' csv header delimiter ';';
 
 -- Inserindo pilotos na tabela core.pilot para todas as equipes de Fórmula 1 da temporada atual
-copy core.pilot (pilot_id, name, birth_date, country, team_id) from 'd:\temp\f1pilots.csv' csv header delimiter ';';
-select * from pilot;
+copy core.pilot (pilot_id, name, birth_date, country, team_id) from '/tmp/f1pilots.csv' csv header delimiter ';';
+
 -- Inserindo corridas na tabela core.corridas 
-copy core.race (race_id, season_year, qualification_date, race_date, total_laps, country, city) from 'd:\temp\f1races.csv' csv header delimiter ';';
-select * from core.race order by race_id;
+copy core.race (race_id, season_year, qualification_date, race_date, total_laps, country, city) from '/tmp/csv/f1races.csv' csv header delimiter ';';
 
 -- Inserindo tempo das corridas na tabela core.race_time 
-copy core.race_time (race_id, pilot_id, lap, time_miliseconds) from 'd:\temp\f1races_time.csv' csv header delimiter ';';
-select * from core.race_time;
+copy core.race_time (race_id, pilot_id, lap, time_miliseconds) from '/tmp/csv/f1races_time.csv' csv header delimiter ';';
 
-select t.name, p.name, r.season_year, r.country, i.lap, i.time_miliseconds 
-from team t
-inner join pilot p on p.team_id = t.team_id
-inner join race_time i on i.pilot_id = p.pilot_id
-inner join race r on r.race_id = i.race_id;
+
+-- Questão 2
+-- Uma lista contendo o nome de todos os pilotos de fórmula 1.
+select p.name from pilot p;
+
+-- Uma lista contendo o nome de todos os pilotos de fórmula 1 da Espanha ou da Finlândia.
+select p.name from pilot p
+where p.country = 'Espanha' or p.country = 'Finlândia';
+	-- Versão Alternativa 1
+	select p.name from pilot p
+	where p.country in ('Espanha', 'Finlândia');
+
+--Uma lista contendo o nome de  todas as escuderias (team) de fórmula 1.
+select t.name from team t;
+
+-- Uma lista contendo o nome de todos os pilotos que têm menos de 30 anos e o nome de suas escuderias (team).
+-- Dica use as funções age para determinar a idade e extrair o ano com a função extract (year from...). Pesquise!
+select p.name, t.name  
+from pilot p inner join team t
+on p.team_id = t.team_id 
+where extract (year from age(current_date, p.birth_date)) < 30; 
+
+--Uma lista contendo o nome de todos os pilotos, da cidade e do país onde já correram (sem repetição)
+select distinct p.name, r.country, r.city
+from pilot p 
+inner join race_time ra on ra.pilot_id = p.pilot_id
+inner join race r on ra.race_id = r.race_id;
+
+-- Uma lista contendo o nome do piloto, o nome da escuderia, a cidade e o país de cada corrida, 
+-- o número da volta e o seu tempo de cada corrida no ano de 2022 (season_year).
+select distinct p.name as "Pilot", t.name "Team", r.city, r.country, ra.lap, ra.time_miliseconds 
+from pilot p 
+inner join team t on p.team_id = t.team_id
+inner join race_time ra on ra.pilot_id = p.pilot_id
+inner join race r on ra.race_id = r.race_id;
+
+-- Uma lista contendo o nome do piloto, o nome da escuderia, a cidade e o país de cada corrida, 
+-- o número da volta mais lenta e da volta mais rápida.
+select distinct p.name as "Pilot", t.name "Team", r.city, r.country, min(ra.time_miliseconds), max(ra.time_miliseconds)
+from pilot p 
+inner join team t on p.team_id = t.team_id
+inner join race_time ra on ra.pilot_id = p.pilot_id
+inner join race r on ra.race_id = r.race_id
+group by p.name, t.name, r.city, r.country;
+
+-- Questão 3
+-- Apague os tempos da primeira volta das corridas de 2021 (tabela race_time) de todos os pilotos;
+delete from race_time rt where rt.lap = 1 and rt.race_id in (select r.race_id from race r where r.season_year = 2021);
+
+-- Some o tempo de todas as voltas em milissegundos para todos os pilotos em que a escuderia é da França 
+-- no grande prêmio da Itália do ano de 2021.
+select sum(rt.time_miliseconds) from race_time rt
+inner join race r on r.race_id = rt.race_id 
+inner join pilot p on p.pilot_id = rt.pilot_id 
+inner join team t on t.team_id = p.team_id 
+where t.country = 'França' 
+and   r.country = 'Itália'
+and   r.season_year = 2021;
+
+-- Acrescente uma coluna na tabela race para indicar o nome do autódromo (racetrack: varchar(255) not null)
+alter table race add column racetrack varchar(255) not null default '';
+
+-- Apague todos os registros de todas as tabelas.
+
+delete from race_time;
+delete from race;
+delete from pilot;
+delete from team;
+
+-- Apague todas as tabelas.
+drop table race_time, race, pilot, team;
+
+--Apague o banco de dados.
+drop database f1db with(force);
 
 
 
